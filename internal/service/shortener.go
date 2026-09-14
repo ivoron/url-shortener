fbbcae1d7cc1aed6b7c8cd20fd6ba6b23f232c25
Илья Воронов <i.voronov@yclients.tech>
2026-09-14 20:08:24 +0300

    добавлен сервис ссылок

---
+++ internal/service/shortener.go
@@ -0,0 +1,84 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"math/rand"
+	"strings"
+	"time"
+
+	"url-shortener/internal/repository"
+	"url-shortener/internal/model"
+)
+
+const (
+	symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	shortURLLength = 8
+)
+
+type ShortenerService interface {
+	Shorten(ctx context.Context, originalURL string) (*model.URL, error)
+	Resolve(ctx context.Context, shortURL string) (*model.URL, error)
+}
+
+type shortenerService struct {
+	repo repository.URLRepository
+}
+
+func NewShortenerService(repo repository.URLRepository) ShortenerService {
+	return &shortenerService{
+		repo: repo,
+	}
+}
+
+func (s *shortenerService) Shorten(ctx context.Context, originalURL string) (*model.URL, error) {
+	if originalURL == "" {
+		return nil, errors.New("URL is empty")
+	}
+
+	shortURL := generateShortURL()
+
+	url := &model.URL{
+		OriginalURL: originalURL,
+		ShortURL:    shortURL,
+	}
+
+	err := s.repo.Save(ctx, url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return url, nil
+}
+
+func (s *shortenerService) Resolve(ctx context.Context, shortURL string) (*model.URL, error) {
+	url, err := s.repo.GetByShortURL(ctx, shortURL)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if url == nil {
+		return nil, errors.New("URL not found")
+	}
+
+	go func() {
+		bgCtx := context.Background()
+		
+		_ = s.repo.IncrementVisits(bgCtx, url.ShortURL)
+	}()
+
+	return url, nil
+}
+
+func generateShortURL() string {
+	rand.Seed(time.Now().UnixNano())
+	var sb strings.Builder
+
+	for i := 0; i < shortURLLength; i++ {
+		sb.WriteByte(symbols[rand.Intn(len(symbols))])
+	}
+
+	return sb.String()
+}
